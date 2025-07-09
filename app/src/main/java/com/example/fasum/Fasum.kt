2ffa6ebd2bd0675e527c0ee20ed3b209c22419ed
BintangAkmalKurniawan <bintangakmal1206@gmail.com>
2025-07-09 09:09:59 +0700

    layout 2

---
+++ app/src/main/java/com/example/fasum/Fasum.kt
@@ -0,0 +1,10 @@
+package com.example.fasum
+
+import java.io.Serializable
+
+class Fasum (
+    var id: String? =null,
+    var email: String? = "",
+    var password: String? ="",
+
+): Serializable
